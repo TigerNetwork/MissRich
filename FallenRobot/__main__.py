a6a1f7666133fd8ee6f2ddb0b7ef51d5f61d5089
Tɪɢᴇʀ Nᴇᴛᴡᴏʀᴋ™ <111215574+TigerNetwork@users.noreply.github.com>
2022-09-18 06:19:12 +0530

    just commit

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -228,7 +228,7 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAJYsmLWRvm70cE-mmxSNCovEf4v1ueJAAIcCAACbMK4VuL4EmZEkq8WKQQ"
+                "CAACAgUAAxkBAAIcjmMmajuYwqvLN-iPOVYZwcsiD6BcAAKwBQACTOE4VcQJP6TLB0FiKQQ"
             )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
