dbdec2bb81f2a9e560ab01dd0c5ef6f1dc584542
HNY <101098354+HnY-xD@users.noreply.github.com>
2022-09-30 15:32:57 +0530

    Update __init__.py

--- EmikoRobot/modules/sql/__init__.py
+++ EmikoRobot/modules/sql/__init__.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-from EmikoRobot import DB_URL as DB_URI
 
 from EmikoRobot import DB_URI
 from EmikoRobot import LOGGER as log
+from EmikoRobot import DB_URL as DB_URI
 
 if DB_URI and DB_URI.startswith("postgres://"):
     DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
